afcc3f97f871e1aee9115e8c10ad393c4afdb796
unknown <you@example.com>
2021-07-24 23:26:25 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from "react-native"
+import Sound from "react-native-sound"
+// import Sound from "react-native-sound"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const soundList = [
+  require("./assets/one.wav"),
+  require("./assets/two.wav"),
+  require("./assets/three.wav"),
+  require("./assets/four.wav"),
+  require("./assets/five.wav"),
+  require("./assets/six.wav"),
+  require("./assets/seven.wav"),
+  require("./assets/eight.wav"),
+  require("./assets/nine.wav"),
+  require("./assets/ten.wav"),
+]
+
+const App = () => {
+  const playSound = (sound) =>{
+    const soundV = new Sound(sound, Sound.MAIN_BUNDLE, (err) =>{
+      if(err){
+        console.log("Not able to play the sound")
+      }
+    });
+    setTimeout(()=>{
+      soundV.play();
+    }, 1000);
+
+    soundV.release();
+  };
+  
+  return(
+    <ScrollView style={styles.container}>
+      <Image style={styles.logo} source={require("./assets/logo.png")} />
+
+      <View style={styles.gridContainer}>
+        {soundList.map((sound)=>(
+
+          <TouchableOpacity style={styles.box} key={sound} onPress={()=>{playSound(sound)}}>
+            <Text style={styles.text}>{sound}</Text>
+          </TouchableOpacity>
+
+        ))}
+      </View>
+    </ScrollView>
+  )
 }
 
+export default App;
+
+
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#141E61"
   },
-});
+  logo:{
+    alignSelf: "center",
+    marginTop: 40,
+  },
+  gridContainer:{
+    flex: 1,
+    margin: 20,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "flex-start",
+    justifyContent: "space-around"
+  },
+  box:{
+    height: 80,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "44%",
+    marginVertical: 6,
+    backgroundColor: "#000",
+    borderRadius: 10,
+    elevation: 5,
+  },
+  text:{
+    fontSize: 30,
+    color:"#CDF3A2"
+  }
+})
